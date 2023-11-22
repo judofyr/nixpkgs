418663c11795f47a0e60af5430c5019a6ba07258
John Ericson <John.Ericson@Obsidian.Systems>
2023-11-22 16:02:24 -0500

    http-parser: Build on windows
    It actually does without modification, but what it installs isn't quite
    right. This fixes that.
    
    The package is no longer maintained, so there isn't really anything to
    upstream here.
    
    (cherry picked from commit 1a7810b863092938f4107373ac3e0de0fe64cc73)

--- pkgs/development/libraries/http-parser/default.nix
+++ pkgs/development/libraries/http-parser/default.nix
@@ -12,6 +12,7 @@ stdenv.mkDerivation rec {
   };
 
   env.NIX_CFLAGS_COMPILE = "-Wno-error";
+
   patches = [
     ./build-shared.patch
   ] ++ lib.optionals stdenv.isAarch32 [
@@ -21,16 +22,33 @@ stdenv.mkDerivation rec {
       sha256 = "sha256-rZZMJeow3V1fTnjadRaRa+xTq3pdhZn/eJ4xjxEDoU4=";
     })
   ];
-  makeFlags = [ "DESTDIR=" "PREFIX=$(out)" ];
+
+  makeFlags = [
+    "DESTDIR=" "PREFIX=$(out)"
+  ] ++ lib.optionals stdenv.hostPlatform.isWindows [
+    "Platform=wine"
+    "SOEXT=dll"
+    "BINEXT=.exe"
+    "SONAME=$(SOLIBNAME).$(SOMAJOR).$(SOMINOR).$(SOEXT)"
+    "LIBNAME=$(SOLIBNAME).$(SOMAJOR).$(SOMINOR).$(SOREV).$(SOEXT)"
+    "LDFLAGS=-Wl,--out-implib=$(LIBNAME).a"
+  ];
+
   buildFlags = [ "library" ];
+
   doCheck = true;
   checkTarget = "test";
+
+  postInstall = lib.optionalString stdenv.hostPlatform.isWindows ''
+    install -D *.dll.a $out/lib
+    ln -sf libhttp_parser.${version}.dll.a $out/lib/libhttp_parser.dll.a
+  '';
 
   meta = with lib; {
     description = "An HTTP message parser written in C";
     homepage = "https://github.com/nodejs/http-parser";
     maintainers = with maintainers; [ matthewbauer ];
     license = licenses.mit;
-    platforms = platforms.unix;
+    platforms = platforms.all;
   };
 }
